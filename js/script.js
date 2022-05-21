a0a0882f09b337cab0c1fa1d3789dff285adea24
MECOZZI Manuel <manuel.mecozzi@farmalink.com.ar>
2022-05-21 01:20:14 -0300

    diseño

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("jelou");
+function calcularPrecio (){
+    document.getElementById("elresult").innerHTML = document.getElementById("valorcanti") + document.getElementById("valorcanti")
+
+    
+
+
+
+
+}
