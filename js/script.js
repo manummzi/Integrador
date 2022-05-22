6ea74299d128b64ce68a767514b7f1e7db8f4cab
MECOZZI Manuel <manuel.mecozzi@farmalink.com.ar>
2022-05-21 23:19:50 -0300

    calcularprecioOK

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
 console.log("jelou");
+
 function calcularPrecio (){
-    document.getElementById("elresult").innerHTML = document.getElementById("valorcanti") + document.getElementById("valorcanti")
+    let resultado1 = document.getElementById("valorcantidad").value;
+   
+    let x = document.getElementById("valortipo").value;
+    switch (x) {
+          
+        case "Estudiante":
+            document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.2);
+            //alert(resultado1 * 200 * 0.2);
+            break;
+        case "1":
+            document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.5);
+            break;
+        case "2":
+            document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.85);
+            break;        
+  
+}
 
-    
-
-
-
-
+   
 }
