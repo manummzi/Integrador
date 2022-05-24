dc35f6cf214a00c4284a7eac1df12d8f8a957b44
MECOZZI Manuel <manuel.mecozzi@farmalink.com.ar>
2022-05-24 17:31:48 -0300

    resumen falta style

--- js/script.js
+++ js/script.js
@@ -7,18 +7,30 @@ function calcularPrecio (){
           
         case "Estudiante":
             document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.2);
-            //alert(resultado1 * 200 * 0.2);
+            document.getElementById("resumCant").innerHTML = ("Cantidad de entradas: " + resultado1) ;
+            document.getElementById("resumCat").innerHTML = "Categoría: Estudiante";
+            document.getElementById("resumTot").innerHTML = ("Total a Pagar: $"+(resultado1 * 200 * 0.2));
+            
             break;
         case "1":
             document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.5);
+            document.getElementById("resumCant").innerHTML = ("Cantidad de entradas: " + resultado1) ;
+            document.getElementById("resumCat").innerHTML = "Categoría: Trainee";
+            document.getElementById("resumTot").innerHTML = ("Total a Pagar: $"+(resultado1 * 200 * 0.5));
             break;
         case "2":
             document.getElementById("elresult").innerHTML = (resultado1 * 200 * 0.85);
+            document.getElementById("resumCant").innerHTML = ("Cantidad de entradas: " + resultado1) ;
+            document.getElementById("resumCat").innerHTML = "Categoría: Junior";
+            document.getElementById("resumTot").innerHTML =  ("Total a Pagar: $"+(resultado1 * 200 * 0.85));
             break;  
 }   
 }
 function calcularPrecio2 (){
     document.getElementById("elresult").innerHTML = " " ;
+    document.getElementById("resumCant").innerHTML = "Cantidad de entradas: " ;
+    document.getElementById("resumCat").innerHTML = "Categoría: ";
+    document.getElementById("resumTot").innerHTML = "Total a Pagar: $";
 }
 function mostrarResumen(){
 
